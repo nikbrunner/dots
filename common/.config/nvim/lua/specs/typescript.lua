acf5e4643dd68149a0d6a440e3e6fe2587d66073
nbr <nikolaus.brunner@protonmail.ch>
2025-08-07 11:00:53 +0200

    feat(nvim): add typescript LSP plugin utilities

---
+++ common/.config/nvim/lua/specs/typescript.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "dmmulroy/tsc.nvim",
+        event = "LspAttach",
+        cmd = { "TSC" },
+        opts = {},
+    },
+    {
+        "dmmulroy/ts-error-translator.nvim",
+        event = "LspAttach",
+        opts = {},
+    },
+}
