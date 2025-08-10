ac2d6bee44d0b26427966719b896c4f1a6ec1afa
nbr <nikolaus.brunner@protonmail.ch>
2025-08-10 13:55:56 +0200

    archive(colorschemes): remove github-nvim-theme and rose-pine

--- common/.config/nvim/lua/specs/colorschemes.lua
+++ common/.config/nvim/lua/specs/colorschemes.lua
@@ -18,49 +18,4 @@ return {
             },
         },
     },
-
-    {
-        "projekt0n/github-nvim-theme",
-        lazy = false,
-        priority = 1000,
-        ---@type GhTheme.Config
-        opts = {
-            options = {
-                hide_end_of_buffer = true,
-                styles = {
-                    comments = "italic",
-                },
-                darken = {
-                    floats = true,
-                    sidebars = {
-                        enable = true,
-                    },
-                },
-            },
-            groups = {
-                all = {
-                    FzfLuaNormal = { bg = "bg0" },
-                    FzfLuaBorder = { bg = "bg0" },
-                    NormalFloat = { bg = "bg0" },
-                    FloatTitle = { bg = "bg0" },
-                    FloatBorder = { bg = "bg0" },
-                },
-            },
-        },
-        config = function(_, opts)
-            require("github-theme").setup(opts)
-        end,
-    },
-
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            highlight_groups = {
-                EndOfBuffer = { fg = "surface" },
-            },
-        },
-    },
 }
