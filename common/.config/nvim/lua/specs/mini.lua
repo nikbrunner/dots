11a193fd1944b8a9aa30594a9408ee2659fb3020
nbr <nikolaus.brunner@protonmail.ch>
2025-08-10 20:27:57 +0200

    feat(mini.files): improve mini.files keymaps and file navigation

--- common/.config/nvim/lua/specs/mini.lua
+++ common/.config/nvim/lua/specs/mini.lua
@@ -19,7 +19,7 @@ function Mini.files()
             mark_set = "m",
             reset = "<BS>",
             reveal_cwd = "@",
-            synchronize = "<C-s>",
+            synchronize = "w",
             trim_left = "<",
             trim_right = ">",
         },
@@ -58,6 +58,39 @@ function Mini.files()
         pattern = "MiniFilesActionRename",
         callback = function(event)
             Snacks.rename.on_rename_file(event.data.from, event.data.to)
+        end,
+    })
+
+    -- Set focused directory as current working directory
+    local set_cwd = function()
+        local path = (MiniFiles.get_fs_entry() or {}).path
+        if path == nil then
+            return vim.notify("Cursor is not on valid entry")
+        end
+        vim.fn.chdir(vim.fs.dirname(path))
+    end
+
+    -- Yank in register full path of entry under cursor
+    local yank_path = function()
+        local path = (MiniFiles.get_fs_entry() or {}).path
+        if path == nil then
+            return vim.notify("Cursor is not on valid entry")
+        end
+        vim.fn.setreg(vim.v.register, path)
+    end
+
+    -- Open path with system default handler (useful for non-text files)
+    local ui_open = function()
+        vim.ui.open(MiniFiles.get_fs_entry().path)
+    end
+
+    vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function(args)
+            local b = args.data.buf_id
+            vim.keymap.set("n", ".", set_cwd, { buffer = b, desc = "Set cwd" })
+            vim.keymap.set("n", "gx", ui_open, { buffer = b, desc = "OS open" })
+            vim.keymap.set("n", "gy", yank_path, { buffer = b, desc = "Yank path" })
         end,
     })
 
