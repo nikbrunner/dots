c237399d6f46c134fbaf5f710381907afcf200bf
nbr <nikolaus.brunner@protonmail.ch>
2025-08-14 14:28:29 +0200

    feat(oil): enhance file explorer with better keymaps and trash support
    - Add delete to trash functionality with gri3ve/oil-trash.nvim
    - Configure intuitive keymaps for file operations
    - Add quick parent navigation with backspace
    - Add toggle hidden files with 'g.'
    - Add file detail view with 'g\\'
    - Improve overall navigation experience

--- common/.config/nvim/lua/specs/oil.lua
+++ common/.config/nvim/lua/specs/oil.lua
@@ -1,6 +1,9 @@
 ---@type LazySpec
 return {
     "stevearc/oil.nvim",
+    dependencies = {
+        { "echasnovski/mini.icons", opts = {} },
+    },
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {
@@ -10,33 +13,69 @@ return {
             prompt_save_on_select_new_entry = false,
         },
         keymaps = {
+            ["q"] = { "actions.close", mode = "n" },
+            ["mh"] = "<cmd>edit $HOME<CR>",
+            ["mr"] = "<cmd>edit $HOME/repos/<CR>",
+            ["md"] = "<cmd>edit $HOME/repos/nikbrunner/dots<CR>",
+            ["mn"] = "<cmd>edit $HOME/repos/nikbrunner/notes<CR>",
             ["<C-v>"] = { "actions.select", opts = { vertical = true } },
             ["<C-s>"] = { "actions.select", opts = { horizontal = true } },
             ["<C-t>"] = { "actions.select", opts = { tab = true } },
-            ["<C-p>"] = "actions.preview",
+        },
+        lsp_file_methods = {
+            -- Enable or disable LSP file operations
+            enabled = true,
+            -- Time to wait for LSP file operations to complete before skipping
+            timeout_ms = 1000,
+            -- Set to true to autosave buffers that are updated with LSP willRenameFiles
+            -- Set to "unmodified" to only save unmodified buffers
+            autosave_changes = true,
+        },
+        win_options = {
+            winbar = "%{v:lua.require('oil').get_current_dir()}",
+        },
+        float = {
+            padding = 5,
+            max_width = 0.35,
+            max_height = 0.5,
+            border = "solid",
+            win_options = {
+                winblend = 10,
+            },
+        },
+        confirmation = {
+            min_width = { 40, 0.35 },
+            max_width = 0.65,
+            max_height = 0.5,
+            min_height = { 5, 0.1 },
+            border = "solid",
+            win_options = {
+                winblend = 10,
+            },
+        },
+        keymaps_help = {
+            border = "solid",
         },
     },
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
     keys = {
         {
-            "-",
+            "<leader>we",
             function()
                 require("oil").open()
             end,
             desc = "[E]xplorer",
         },
     },
-    config = function(_, opts)
-        require("oil").setup(opts)
-
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "OilActionsPost",
-            callback = function(event)
-                if event.data.actions.type == "move" then
-                    Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
-                end
-            end,
-        })
-    end,
+    -- config = function(_, opts)
+    --     require("oil").setup(opts)
+    --
+    --     vim.api.nvim_create_autocmd("User", {
+    --         pattern = "OilActionsPost",
+    --         callback = function(event)
+    --             if event.data.actions.type == "move" then
+    --                 Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+    --             end
+    --         end,
+    --     })
+    -- end,
 }
-
