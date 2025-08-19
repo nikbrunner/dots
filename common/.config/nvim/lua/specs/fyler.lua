30b989bbcd00aae8f38c23b140d9f4bfc568bb2d
nbr <nikolaus.brunner@protonmail.ch>
2025-08-19 18:26:03 +0200

    feat(nvim): add fyler and yazi file explorer plugins

---
+++ common/.config/nvim/lua/specs/fyler.lua
@@ -0,0 +1,68 @@
+---@diagnostic disable: missing-fields
+return {
+    "A7Lavinraj/fyler.nvim",
+    dependencies = { "echasnovski/mini.icons" },
+    ---@type FylerConfig
+    opts = {
+        hooks = {
+            on_rename = function(src_path, dst_path)
+                Snacks.rename.rename_file({ from = src_path, to = dst_path })
+            end,
+        },
+        mappings = {
+            explorer = {
+                ["q"] = "CloseView",
+                ["<CR>"] = "Select",
+                ["<C-t>"] = "SelectTab",
+                ["<C-v>"] = "SelectVSplit",
+                ["<C-s>"] = "SelectSplit",
+                ["-"] = "GotoParent",
+                ["="] = "GotoCwd",
+                ["."] = "GotoNode",
+            },
+            confirm = {
+                ["y"] = "Confirm",
+                ["n"] = "Discard",
+            },
+        },
+        views = {
+            confirm = {
+                border = "solid",
+                win_opts = {
+                    winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,FloatTitle:FloatTitle",
+                    wrap = false,
+                },
+            },
+            explorer = {
+                close_on_select = true,
+                win = {
+                    border = "solid",
+                    win_opts = {
+                        winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,FloatTitle:FloatTitle",
+                        wrap = false,
+                    },
+                    kind_presets = {
+                        float = {
+                            height = "0.6rel",
+                            width = "0.2rel",
+                            top = "0.1rel",
+                            left = "0.1rel",
+                        },
+                        split_left_most = {
+                            width = "0.2rel",
+                        },
+                    },
+                },
+            },
+        },
+    },
+    keys = {
+        {
+            "-",
+            function()
+                require("fyler").open({ cwd = vim.fn.getcwd(), kind = "float" })
+            end,
+            desc = "Fyler",
+        },
+    },
+}
