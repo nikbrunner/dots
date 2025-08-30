a0601f44c8c79c0d221dae7d01ec7c8872e8f942
nbr <nikolaus.brunner@protonmail.ch>
2025-08-30 11:05:53 +0200

    refactor(nvim_mnml): update minimal neovim configuration

--- common/.config/nvim_mnml/init.lua
+++ common/.config/nvim_mnml/init.lua
@@ -1,3 +1,14 @@
+-- Profiler setup (must be first!)
+if vim.env.PROF then
+  -- Load snacks from opt directory
+  vim.cmd("packadd snacks.nvim")
+  require("snacks.profiler").startup({
+    startup = {
+      event = "VimEnter", -- stop profiler on this event
+    },
+  })
+end
+
 require("options")
 require("keymaps")
 require("auto")
