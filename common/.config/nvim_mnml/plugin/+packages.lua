feac1d2511ae373f0aa7466b6cb4e6a408e90928
nbr <nikolaus.brunner@protonmail.ch>
2025-08-30 11:05:59 +0200

    refactor(nvim_mnml): update packages configuration

--- common/.config/nvim_mnml/plugin/+packages.lua
+++ common/.config/nvim_mnml/plugin/+packages.lua
@@ -1,6 +1,7 @@
 vim.pack.add({
     { src = "https://github.com/black-atom-industries/nvim", name = "black-atom" },
     { src = "https://github.com/stevearc/conform.nvim" },
+    { src = "https://github.com/folke/snacks.nvim" },
     { src = "https://github.com/lewis6991/gitsigns.nvim" },
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/echasnovski/mini.nvim" },
