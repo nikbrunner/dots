2ebcb28a832c0291f4cf25fc4f650d8970fc6b2b
nbr <nikolaus.brunner@protonmail.ch>
2025-08-30 11:02:38 +0200

    fix(nvim): update yazi integration configuration

--- common/.config/nvim/lua/specs/yazi.lua
+++ common/.config/nvim/lua/specs/yazi.lua
@@ -1,9 +1,16 @@
 return {
     "mikavilpas/yazi.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    enabled = false,
+    enabled = true,
     event = "VeryLazy",
     keys = {
+        {
+            "<leader>we",
+            function()
+                require("yazi").yazi()
+            end,
+            desc = "[E]xplorer",
+        },
         {
             "-",
             function()
@@ -22,10 +29,10 @@ return {
 
     ---@type YaziConfig
     opts = {
-        yazi_floating_window_winblend = 10,
+        yazi_floating_window_winblend = 0,
         floating_window_scaling_factor = {
-            height = vim.o.lines,
-            width = vim.o.columns,
+            height = 0.8,
+            width = 0.8,
         },
         yazi_floating_window_border = "solid",
     },
