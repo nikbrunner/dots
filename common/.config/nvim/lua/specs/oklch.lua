b2341277a04d40685b6c2383cea395e0ede87b5c
nbr <nikolaus.brunner@protonmail.ch>
2025-09-29 15:20:54 +0200

    feat(nvim/colorpicker): switch from ccc to oklch color picker

---
+++ common/.config/nvim/lua/specs/oklch.lua
@@ -0,0 +1,24 @@
+return {
+    "eero-lehtinen/oklch-color-picker.nvim",
+    event = "VeryLazy",
+    version = "*",
+    keys = {
+        {
+            "<leader>ac",
+            function()
+                require("oklch-color-picker").pick_under_cursor()
+            end,
+            desc = "Color pick under cursor",
+        },
+    },
+    ---@type oklch.Opts
+    opts = {
+        highlight = {
+            enabled = true,
+            ---@type 'background'|'foreground'|'virtual_left'|'virtual_eol'|'foreground+virtual_left'|'foreground+virtual_eol'
+            style = "foreground+virtual_left",
+            bold = true,
+            virtual_text = " ",
+        },
+    },
+}
