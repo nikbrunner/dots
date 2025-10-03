bf8dc74b442f26f5003cf74227329198d3da1a72
nbr <nikolaus.brunner@protonmail.ch>
2025-10-03 13:01:32 +0200

    feat(nvim/completion): adjust completion menu auto-show delay to 350ms

--- common/.config/nvim/lua/specs/blink.lua
+++ common/.config/nvim/lua/specs/blink.lua
@@ -66,7 +66,7 @@ return {
             menu = {
                 border = "solid",
                 winblend = 10,
-                auto_show_delay_ms = 500,
+                auto_show_delay_ms = 350,
                 draw = {
                     columns = {
                         { "kind_icon", gap = 1 },
