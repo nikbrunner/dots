dbf86c7237f9506f9ec21228771c2dfcb26876fa
nbr <nikolaus.brunner@protonmail.ch>
2025-10-04 19:00:57 +0200

    feat(nvim/oklch): add color picker plugin with key reference comment

--- common/.config/nvim/lua/specs/oklch.lua
+++ common/.config/nvim/lua/specs/oklch.lua
@@ -1,3 +1,16 @@
+---https://github.com/eero-lehtinen/oklch-color-picker/blob/bd1cc2b2ab1e9416861af6d7329e5399aeba5cc1/src/app.rs#L871
+-- let keys = [
+--     ("q", "Quit"),
+--     ("c", "Copy to clipboard"),
+--     ("d", "Done (print result to console)"),
+--     ("←/↓/↑/→", "Move focus or control input"),
+--     ("h/j/k/l", "Move focus or control input (Vim style)"),
+--     ("1/2", "Switch focus to pickers"),
+--     ("3/4/5/6", "Switch focus to sliders"),
+--     ("Tab/S-Tab", "Cycle focus"),
+--     ("Esc/Enter", "Back/Submit"),
+-- ];
+
 return {
     "eero-lehtinen/oklch-color-picker.nvim",
     event = "VeryLazy",
