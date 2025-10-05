cbf5cb028852fc2a1eade8837f4fad9d212ab4af
nbr <nikolaus.brunner@protonmail.ch>
2025-10-05 19:15:51 +0200

    chore(nvim/plugins): disable radar.nvim plugin

--- common/.config/nvim/lua/specs/black-atom.lua
+++ common/.config/nvim/lua/specs/black-atom.lua
@@ -23,8 +23,10 @@ return {
     },
     {
         "black-atom-industries/radar.nvim",
-        dir = require("lib.config").get_repo_path("black-atom-industries/radar.nvim"),
-        lazy = false,
+        -- dir = require("lib.config").get_repo_path("black-atom-industries/radar.nvim"),
+        -- lazy = false,
+        enabled = false,
+        event = "VimEnter",
         ---@module "radar"
         ---@type Radar.Config
         opts = {},
