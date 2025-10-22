d7bdde3e6335103565c78056768f48dba64a3163
nbr <nikolaus.brunner@protonmail.ch>
2025-10-22 14:50:56 +0200

    feat(nvim/specs): add supermaven configuration

---
+++ common/.config/nvim/lua/specs/supermaven.lua
@@ -0,0 +1,12 @@
+---@type LazyPluginSpec
+return {
+    "supermaven-inc/supermaven-nvim",
+    event = "InsertEnter",
+    opts = {
+        keymaps = {
+            accept_suggestion = "<Tab>",
+            accept_word = "<S-Tab>",
+            clear_suggestion = "<Left>",
+        },
+    },
+}
