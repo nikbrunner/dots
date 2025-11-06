01d5009a480115c071eeef17691281c1f1559eb4
nbr <nikolaus.brunner@protonmail.ch>
2025-11-06 10:21:20 +0100

    feat(nvim/supermaven): add toggle function and keybind

--- common/.config/nvim/lua/specs/supermaven.lua
+++ common/.config/nvim/lua/specs/supermaven.lua
@@ -1,5 +1,20 @@
+local M = {}
+
+M.toggle_inline_completion = function()
+    local suggestion = require("supermaven-nvim.completion_preview")
+    local message = "AI Auto-Completion "
+
+    if suggestion.disable_inline_completion then
+        suggestion.disable_inline_completion = false
+        vim.notify(message .. "ENABLED", vim.log.levels.INFO, { title = "SuperMaven" })
+    else
+        suggestion.disable_inline_completion = true
+        vim.notify(message .. "DISABLED", vim.log.levels.INFO, { title = "SuperMaven" })
+    end
+end
+
 ---@type LazyPluginSpec
-return {
+M.spec = {
     "supermaven-inc/supermaven-nvim",
     event = "InsertEnter",
     opts = {
@@ -8,5 +23,16 @@ return {
             accept_word = "<S-Tab>",
             clear_suggestion = "<Left>",
         },
+        log_level = "off", -- set to "off" to disable logging completely
     },
+    keys = {
+        { "<leader>aoa", M.toggle_inline_completion, desc = "[A]uto-Completion" },
+    },
+    config = function(_, opts)
+        require("supermaven-nvim").setup(opts)
+        -- Disable inline completion by default
+        require("supermaven-nvim.completion_preview").disable_inline_completion = false
+    end,
 }
+
+return M.spec
