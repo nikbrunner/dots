6bbafd6f3e309d9aef1b366c8795317b46a5ed6c
nbr <nikolaus.brunner@protonmail.ch>
2025-11-12 09:57:01 +0100

    feat(nvim/lsp): add astro to cssls filetypes

--- common/.config/nvim/lsp/cssls.lua
+++ common/.config/nvim/lsp/cssls.lua
@@ -4,7 +4,7 @@
 ---@type vim.lsp.Config
 return {
     cmd = { "vscode-css-language-server", "--stdio" },
-    filetypes = { "css", "scss", "less" },
+    filetypes = { "css", "scss", "less", "astro" },
     settings = {
         css = { validate = true },
         scss = { validate = true },
