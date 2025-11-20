f36f669088ba3e3fcfa07527da15618e2375351c
nbr <nikolaus.brunner@protonmail.ch>
2025-11-20 10:33:53 +0100

    chore: update colorscheme to black-atom-stations-operations

--- common/.config/nvim/lua/config.lua
+++ common/.config/nvim/lua/config.lua
@@ -3,7 +3,7 @@ local M = {}
 ---@class VinConfig
 M.config = {
     ---@type BlackAtom.Theme.Key
-    colorscheme = "black-atom-mnml-clay-dark",
+    colorscheme = "black-atom-stations-operations",
     dev_mode = true,
     date_format = "%Y.%m.%d - %A",
     pathes = {
