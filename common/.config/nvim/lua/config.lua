4d2e0aac7fce3f199c4488742d65b2febc609a25
nbr <nikolaus.brunner@protonmail.ch>
2026-01-02 15:05:45 +0100

    config(themes): add black-atom default themes

--- common/.config/nvim/lua/config.lua
+++ common/.config/nvim/lua/config.lua
@@ -3,7 +3,7 @@ local M = {}
 ---@class VinConfig
 M.config = {
     ---@type BlackAtom.Theme.Key
-    colorscheme = "black-atom-stations-engineering",
+    colorscheme = "black-atom-default-dark-dimmed",
     dev_mode = true,
     date_format = "%Y.%m.%d - %A",
     pathes = {
