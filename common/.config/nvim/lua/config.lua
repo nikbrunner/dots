90e6e474642bcc938bd5eb1b4d82fdb1e7cee010
nbr <nikolaus.brunner@protonmail.ch>
2026-01-13 13:08:43 +0100

    chore(themes): switch to black-atom-terra-fall-day
    Update theme across ghostty, nvim, tmux, and zed editors.
    Switch gitconfig delta to light mode.

--- common/.config/nvim/lua/config.lua
+++ common/.config/nvim/lua/config.lua
@@ -3,7 +3,7 @@ local M = {}
 ---@class VinConfig
 M.config = {
     ---@type BlackAtom.Theme.Key
-    colorscheme = "black-atom-default-dark",
+    colorscheme = "black-atom-terra-fall-day",
     dev_mode = true,
     date_format = "%Y.%m.%d - %A",
     pathes = {
