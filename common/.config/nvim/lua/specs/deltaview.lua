e4aa4bbf27cdd5ea3d9a02872fceafd1e9e3bcdc
nbr <nikolaus.brunner@protonmail.ch>
2026-01-14 15:43:21 +0100

    feat(nvim): add CodeDiff keybindings for AWDCS integration

--- common/.config/nvim/lua/specs/deltaview.lua
+++ common/.config/nvim/lua/specs/deltaview.lua
@@ -2,6 +2,24 @@ return {
     "esmuellert/codediff.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
     cmd = "CodeDiff",
-    event = "VeryLazy",
+    keys = {
+        -- Workspace level
+        { "<leader>wvD", "<cmd>CodeDiff<cr>", desc = "Workspace [D]iff (CodeDiff)" },
+        {
+            "<leader>wvB",
+            function()
+                vim.ui.input({ prompt = "Compare against branch: " }, function(branch)
+                    if branch and branch ~= "" then
+                        vim.cmd("CodeDiff " .. branch)
+                    end
+                end)
+            end,
+            desc = "Compare [B]ranch (CodeDiff)",
+        },
+        { "<leader>wvM", "<cmd>CodeDiff merge<cr>", desc = "[M]erge conflicts (CodeDiff)" },
+
+        -- Document level
+        { "<leader>dvD", "<cmd>CodeDiff file HEAD<cr>", desc = "Document [D]iff (CodeDiff)" },
+    },
     opts = {},
 }
