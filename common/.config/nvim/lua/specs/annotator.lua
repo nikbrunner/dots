d0574d0392a84fb99db973697935ac395f7a011c
nbr <nikolaus.brunner@protonmail.ch>
2026-05-05 19:48:23 +0200

    feat(nvim): add annotator.nvim configuration

---
+++ common/.config/nvim/lua/specs/annotator.lua
@@ -0,0 +1,57 @@
+return {
+    "chpeters/annotator.nvim",
+    event = "VeryLazy",
+    keys = {
+        -- Document → Annotate
+        {
+            "<leader>daa",
+            function()
+                require("annotator").add()
+            end,
+            desc = "[A]dd",
+            mode = { "n", "v" },
+        },
+        {
+            "<leader>das",
+            function()
+                require("annotator").suggest()
+            end,
+            desc = "[S]uggest rewrite",
+            mode = { "n", "v" },
+        },
+        -- { "<leader>dam", function() require("annotator").mark_delete() end, desc = "[M]ark for deletion", mode = { "n", "v" } },
+        {
+            "<leader>dad",
+            function()
+                require("annotator").delete()
+            end,
+            mode = { "n", "v" },
+            desc = "[D]elete annotation",
+        },
+        {
+            "<leader>dae",
+            function()
+                require("annotator").edit()
+            end,
+            desc = "[E]dit annotation",
+        },
+        {
+            "<leader>dal",
+            function()
+                require("annotator").list()
+            end,
+            desc = "[L]ist annotations",
+        },
+        {
+            "<leader>daE",
+            function()
+                require("annotator").export()
+            end,
+            desc = "[E]xport annotations",
+        },
+    },
+    opts = {
+        mappings = false,
+        storage = "memory",
+    },
+}
