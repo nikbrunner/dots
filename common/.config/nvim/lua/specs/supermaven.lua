0d2ece0feb2e8aead74de0926f26dfb93eb85447
nbr <nikolaus.brunner@protonmail.ch>
2026-05-11 09:36:57 +0200

    fix(nvim): update blink and supermaven keybindings

--- common/.config/nvim/lua/specs/supermaven.lua
+++ common/.config/nvim/lua/specs/supermaven.lua
@@ -19,9 +19,9 @@ M.spec = {
     event = "InsertEnter",
     opts = {
         keymaps = {
-            accept_suggestion = "<Tab>",
-            accept_word = "<S-Tab>",
-            clear_suggestion = "<Left>",
+            accept_suggestion = "<C-y>",
+            accept_word = "<M-]>",
+            clear_suggestion = "<M-]>",
         },
         log_level = "off", -- set to "off" to disable logging completely
     },
@@ -30,6 +30,19 @@ M.spec = {
     },
     config = function(_, opts)
         require("supermaven-nvim").setup(opts)
+
+        -- Also accept with <C-y> (blink handles <C-y> when its menu is visible;
+        -- otherwise this accepts the supermaven ghost text, falling through
+        -- to default <C-y> when there's no suggestion).
+        vim.keymap.set("i", "<C-y>", function()
+            local cp = require("supermaven-nvim.completion_preview")
+            if cp.has_suggestion() then
+                cp.on_accept_suggestion()
+            else
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-y>", true, false, true), "n", false)
+            end
+        end, { desc = "Accept suggestion" })
+
         -- Disable inline completion by default
         require("supermaven-nvim.completion_preview").disable_inline_completion = false
     end,
