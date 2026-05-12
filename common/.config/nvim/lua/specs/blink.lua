8da6ab30bf58a6e6782265eb86fca34bd8b948c5
nbr <nikolaus.brunner@protonmail.ch>
2026-05-12 17:22:34 +0200

    feat(nvim): enable blink auto_insert on completion

--- common/.config/nvim/lua/specs/blink.lua
+++ common/.config/nvim/lua/specs/blink.lua
@@ -51,7 +51,7 @@ return {
             list = {
                 selection = {
                     preselect = false,
-                    auto_insert = false,
+                    auto_insert = true,
                 },
             },
             accept = {
