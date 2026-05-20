4c4b5017feae4509324b716eb8503b995352d8db
nbr <nikolaus.brunner@protonmail.ch>
2026-05-20 18:10:49 +0200

    chore(nvim): remove sourcekit lsp configuration

--- common/.config/nvim/lsp/sourcekit.lua
+++
@@ -1,8 +0,0 @@
--- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/sourcekit.lua
-
----@type vim.lsp.Config
-return {
-    cmd = { "sourcekit-lsp" },
-    filetypes = { "swift", "objc", "objcpp", "c", "cpp" },
-    root_markers = { "buildServer.json", ".bsp", "*.xcodeproj", "*.xcworkspace", "Package.swift", ".git" },
-}
