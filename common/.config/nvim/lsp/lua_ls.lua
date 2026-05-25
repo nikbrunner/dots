65d0eeb567c53b33a5f50919b14c1a9d31bbb278
nbr <nikolaus.brunner@protonmail.ch>
2026-05-25 15:42:31 +0200

    chore(nvim): suppress Lua LS progress ticks, notify on completion

--- common/.config/nvim/lsp/lua_ls.lua
+++ common/.config/nvim/lsp/lua_ls.lua
@@ -8,6 +8,17 @@ return {
     cmd = { "lua-language-server" },
     filetypes = { "lua" },
     root_markers = { ".luarc.json", ".luarc.jsonc" },
+    handlers = {
+        ["$/progress"] = function(_, params)
+            if params and params.value and params.value.kind == "end" then
+                vim.notify(
+                    ("Lua LS: %s"):format(params.value.message or "ready"),
+                    vim.log.levels.INFO,
+                    { title = "Lua Language Server" }
+                )
+            end
+        end,
+    },
     settings = {
         Lua = {
             -- Using stylua for formatting.
