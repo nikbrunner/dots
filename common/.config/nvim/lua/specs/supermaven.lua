8ae9a14b2221a0fef73b047f4f29a1f4e0b77ca8
nbr <nikolaus.brunner@protonmail.ch>
2026-06-06 13:41:54 +0200

    refactor(supermaven): simplify

--- common/.config/nvim/lua/specs/supermaven.lua
+++ common/.config/nvim/lua/specs/supermaven.lua
@@ -31,19 +31,6 @@ M.spec = {
     config = function(_, opts)
         require("supermaven-nvim").setup(opts)
 
-        -- Also accept with <C-y> (blink handles <C-y> when its menu is visible;
-        -- otherwise this accepts the supermaven ghost text, falling through
-        -- to default <C-y> when there's no suggestion).
-        vim.keymap.set("i", "<C-y>", function()
-            local cp = require("supermaven-nvim.completion_preview")
-            if cp.has_suggestion() then
-                cp.on_accept_suggestion()
-            else
-                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-y>", true, false, true), "n", false)
-            end
-        end, { desc = "Accept suggestion" })
-
-        -- Disable inline completion by default
         require("supermaven-nvim.completion_preview").disable_inline_completion = false
     end,
 }
