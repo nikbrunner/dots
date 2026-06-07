9f91f66e97dc844622a002c5128bb610077bf0e3
nbr <nikolaus.brunner@protonmail.ch>
2026-06-07 18:27:27 +0200

    fix(nvim/edit): wrap Snacks rename call in pcall during file rename

--- common/.config/nvim-edit/plugin/50_specs/mini/files.lua
+++ common/.config/nvim-edit/plugin/50_specs/mini/files.lua
@@ -125,7 +125,7 @@ vim.api.nvim_create_autocmd("User", {
 vim.api.nvim_create_autocmd("User", {
 	pattern = "MiniFilesActionRename",
 	callback = function(event)
-		Snacks.rename.on_rename_file(event.data.from, event.data.to)
+		pcall(Snacks.rename.on_rename_file, event.data.from, event.data.to)
 	end,
 })
 
