6198176d057ab1fb2f2be3934de32f895975a9a3
nbr <nikolaus.brunner@protonmail.ch>
2026-06-07 18:22:41 +0200

    feat(nvim/edit): add supermaven configuration

---
+++ common/.config/nvim-edit/plugin/50_specs/supermaven.lua
@@ -0,0 +1,24 @@
+local function toggle_inline_completion()
+	local suggestion = require("supermaven-nvim.completion_preview")
+	local message = "AI Auto-Completion "
+
+	if suggestion.disable_inline_completion then
+		suggestion.disable_inline_completion = false
+		vim.notify(message .. "ENABLED", vim.log.levels.INFO, { title = "SuperMaven" })
+	else
+		suggestion.disable_inline_completion = true
+		vim.notify(message .. "DISABLED", vim.log.levels.INFO, { title = "SuperMaven" })
+	end
+end
+
+Edit.later(function()
+	vim.pack.add({ "git@github.com:supermaven-inc/supermaven-nvim" })
+
+	require("supermaven-nvim").setup({
+		log_level = "off", -- set to "off" to disable logging completely
+	})
+
+	require("supermaven-nvim.completion_preview").disable_inline_completion = false
+
+	vim.keymap.set("n", "<leader>aoa", toggle_inline_completion, { desc = "[A]uto-Completion" })
+end)
