5c6a45b67edbd38a2f099057cac538de07ae8f2b
nbr <nikolaus.brunner@protonmail.ch>
2026-06-10 09:39:06 +0200

    feat(nvim/edit): add helpview.nvim for prettier :help rendering

---
+++ common/.config/nvim-edit/plugin/50_specs/helpview.lua
@@ -0,0 +1,6 @@
+-- Prettier rendering of :help files (decorations, links, code blocks).
+-- Self-activates on the help filetype; no setup needed.
+
+Edit.later(function()
+	vim.pack.add({ "git@github.com:OXY2DEV/helpview.nvim" })
+end)
