c0da009158cdcdfcf436ff9da001e4dcc9176c6a
nbr <nikolaus.brunner@protonmail.ch>
2026-06-10 09:43:35 +0200

    feat(nvim/edit): add whatthejump.nvim for jumplist preview

---
+++ common/.config/nvim-edit/plugin/50_specs/whatthejump.lua
@@ -0,0 +1,6 @@
+-- Show jumplist neighbors in a float while navigating with <C-o> / <C-i>.
+-- No setup needed; activates on jumplist movement.
+
+Edit.later(function()
+	vim.pack.add({ "git@github.com:lewis6991/whatthejump.nvim" })
+end)
