46e79fc29c183b7322a4835732d1274704bba28c
nbr <nikolaus.brunner@protonmail.ch>
2026-06-12 13:58:15 +0200

    refactor(nvim/edit): configure mini.ai mappings

--- common/.config/nvim-edit/plugin/50_specs/mini/ai.lua
+++ common/.config/nvim-edit/plugin/50_specs/mini/ai.lua
@@ -1,1 +1,10 @@
-require("mini.ai").setup()
+require("mini.ai").setup({
+	mappings = {
+
+		-- TODO: use native vim motions
+		around_next = "",
+		inside_next = "",
+		around_last = "",
+		inside_last = "",
+	},
+})
