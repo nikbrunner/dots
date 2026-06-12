c33eeb71bad04b61e9760ba517a33cd4b10f3e94
nbr <nikolaus.brunner@protonmail.ch>
2026-06-12 14:09:15 +0200

    refactor(nvim/edit): update `iter.nvim` with diffs.nvim

--- common/.config/nvim-edit/plugin/50_specs/black_atom/iter.lua
+++ common/.config/nvim-edit/plugin/50_specs/black_atom/iter.lua
@@ -1,19 +1,13 @@
-Edit.later(function()
-	-- vim.pack.add({ "git@github.com:black-atom-industries/iter.nvim.git" })
+Edit.now(function()
+	vim.pack.add({ "https://github.com/barrettruth/diffs.nvim" })
 
+	-- vim.pack.add({ "git@github.com:black-atom-industries/iter.nvim.git" })
 	vim.opt.rtp:prepend(vim.fn.expand("~/repos/black-atom-industries/iter.nvim"))
 
 	require("iter").setup({
 		preview = {
 			-- Start diff previews with wrapping disabled.
 			wrap = false,
-
-			-- Show old/new line numbers in diff previews.
-			show_line_numbers = true,
-
-			-- Show git diff metadata rows such as `diff --git`, `index`, `---`,
-			-- and `+++`.
-			show_metadata = false,
 
 			-- Diff preview layout: 'stacked', 'split', or 'auto'.
 			diff_layout = "auto",
