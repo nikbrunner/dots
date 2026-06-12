f4068c1d95682d8ab845062051d85399ca77cfd2
nbr <nikolaus.brunner@protonmail.ch>
2026-06-12 17:24:07 +0200

    feat(nvim/edit): enable vim.ui2

--- common/.config/nvim-edit/plugin/10_options/base.lua
+++ common/.config/nvim-edit/plugin/10_options/base.lua
@@ -108,3 +108,29 @@ Edit.new_autocmd('FileType', nil, f, "Proper 'formatoptions'")
 
 -- Diagnostics are configured in 'plugin/10_options/lsp.lua'.
 -- stylua: ignore end
+
+require("vim._core.ui2").enable({
+	enable = true, -- Whether to enable or disable the UI.
+	msg = { -- Options related to the message module.
+		---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
+		---or table mapping |ui-messages| kinds, triggers and IDs to a target.
+		---Table keys are are matched as a Lua pattern to the message ID. 'default'
+		---mapping applies to any omitted kind: { default = 'cmd', progress = 'msg' }.
+		targets = "cmd",
+		cmd = { -- Options related to messages in the cmdline window.
+			-- Maximum height (rows if >=1, or % of 'lines' if <1) of messages expanded
+			-- beyond 'cmdheight'; 0.999 for full height.
+			height = 0.5,
+		},
+		dialog = { -- Options related to dialog window.
+			height = 0.5, -- Maximum height.
+		},
+		msg = { -- Options related to msg window.
+			height = 0.5, -- Maximum height.
+			timeout = 4000, -- Time a message is visible in the message window.
+		},
+		pager = { -- Options related to message window.
+			height = 0.999, -- Maximum height.
+		},
+	},
+})
