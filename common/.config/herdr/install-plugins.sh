d007ea2b7539a0756cc778aa8a15776cb446d072
nbr <nikolaus.brunner@protonmail.ch>
2026-07-12 19:44:58 +0200

    feat(herdr): add plugin install script

---
+++ common/.config/herdr/install-plugins.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# send yes to all questions
+
+# https://github.com/paulbkim-dev/vim-herdr-navigation
+herdr plugin install paulbkim-dev/vim-herdr-navigation
+
+# https://github.com/thanhdat77/herdr-picker-plus
+herdr plugin install thanhdat77/herdr-picker-plus
+
+# https://github.com/cloudmanic/herdr-plus
+herdr plugin install cloudmanic/herdr-plus
+
+# https://github.com/third774/herdr-last-workspace
+herdr plugin install third774/herdr-last-workspace
