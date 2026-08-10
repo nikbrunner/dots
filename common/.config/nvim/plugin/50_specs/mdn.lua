8fbaec229e5336ee2bd49950f6b605b3977cd1b4
nbr <nikolaus.brunner@protonmail.ch>
2026-08-10 10:00:52 +0200

    cleanup mdn plugin spec to remove local dev path comments

--- common/.config/nvim/plugin/50_specs/mdn.lua
+++ common/.config/nvim/plugin/50_specs/mdn.lua
@@ -4,7 +4,9 @@
 -- (e.g. restored from a session before the plugin was on the rtp).
 Edit.on_filetype("markdown", function()
 	vim.pack.add({ "git@github.com:nikbrunner/mdn.nvim" })
-	-- vim.opt.rtp:prepend(vim.fn.expand("~/repos/nikbrunner/mdn.nvim"))
+	-- local dev = vim.fn.expand("~/repos/nikbrunner/mdn.nvim")
+	-- vim.opt.rtp:prepend(dev)
+	-- vim.opt.rtp:append(dev .. "/after")
 
 	require("mdn").setup()
 end)
