b7784b553fb2892df1176b0af7c1ddab61a98697
nbr <nikolaus.brunner@protonmail.ch>
2026-08-29 19:21:43 +0200

    remove herdr reviewr plugin

--- common/.config/herdr/install-plugins.sh
+++ common/.config/herdr/install-plugins.sh
@@ -22,8 +22,8 @@ herdr plugin install aimdevlee/herdr-nvim-nav --yes
 # https://github.com/black-atom-industries/helm.herdr
 herdr plugin install black-atom-industries/helm.herdr --yes
 
-# https://github.com/persiyanov/herdr-reviewr
-herdr plugin install persiyanov/herdr-reviewr --yes
-
 # https://github.com/zenbu-labs/terminal-browser/tree/main/herdr-plugin
 herdr plugin install zenbu-labs/terminal-browser/herdr-plugin --yes
+
+#  https://github.com/plannotator/herdr-annotate
+herdr plugin install plannotator/herdr-annotate --yes
