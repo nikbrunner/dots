15e4c652a65127cfa19b27191aba5cf91fddb3f5
nbr <nikolaus.brunner@protonmail.ch>
2026-09-06 17:28:03 +0200

    Remove hardcoded black-atom path to prevent runtime errors

--- common/.config/nvim/plugin/50_specs/black_atom/nvim.lua
+++ common/.config/nvim/plugin/50_specs/black_atom/nvim.lua
@@ -1,13 +1,4 @@
 Edit.now(function()
-	-- vim.pack.add({
-	-- 	{
-	-- 		src = "git@github.com:nikbrunner/black-atom.git",
-	-- 		name = "black-atom",
-	-- 	},
-	-- })
-	-- Development: comment line above, uncomment below — no other setup needed:
-	vim.opt.rtp:prepend(vim.fn.expand("~/repos/nikbrunner/black-atom/adapters/nvim"))
-
 	---@type BlackAtom.Config
 	vim.g.black_atom_core_config = {
 		styles = {
